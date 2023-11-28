6544279287f2ca622b14ea1afc8c32adacfdfd7b
Yurii Bobal <woeatory@gmail.com>
2023-11-28 16:29:04 +0200

    Gravity fix

--- Assets/Scripts/Player/PlayerMovementBehaviour.cs
+++ Assets/Scripts/Player/PlayerMovementBehaviour.cs
@@ -6,26 +6,44 @@ public class PlayerMovementBehaviour : MonoBehaviour
     [SerializeField] CharacterController characterController;
     [Header("Movement Settings")]
     [SerializeField] float movementSpeed = 10f;
-    private Vector3 movementDirection;
-    
+    [SerializeField] float fallingSpeed = -4.5f;
+    private float _playerYAxisVelocity;
+    private Vector3 _movementDirection;
+    private bool isGrounded;
+
     private void Start()
     {
         characterController = GetComponent<CharacterController>();
     }
     private void Update()
     {
+        ApplyGravity();
         MoveThePlayer();
     }
-    public void MoveThePlayer()
+    private void MoveThePlayer()
     {
-        characterController.Move(movementDirection * movementSpeed * Time.deltaTime);
+        _movementDirection.x *= movementSpeed;
+        _movementDirection.z *= movementSpeed;
+        characterController.Move(_movementDirection * Time.deltaTime);
     }
     public void UpdateMovementData(Vector3 movementDirection)
     {
-        this.movementDirection = transform.right * movementDirection.x + transform.forward * movementDirection.z;
+        _movementDirection = transform.right * movementDirection.x + transform.forward * movementDirection.z;
     }
     public void PerformeJump()
     {
 
+    }
+
+    private void ApplyGravity()
+    {
+        isGrounded = characterController.isGrounded;
+        Debug.Log(isGrounded);
+        if (isGrounded && _playerYAxisVelocity < 0)
+        {
+            _playerYAxisVelocity = -2f;
+        } 
+        _playerYAxisVelocity = fallingSpeed;
+        _movementDirection.y = _playerYAxisVelocity;
     }
 }
